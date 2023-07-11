6ba20579284ebcc3ecc27ef363008dbe9a9fac06
gurpreet991 <gurpreet.singh@ellocentlabs.com>
2023-07-11 15:40:30 +0530

    removed duplicate css

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -29,24 +29,15 @@ const Layout = ({children, element, ...props}) => {
                     media="all" />
                 <link rel="preload" as="style" id="oe-widgets-style-css"
                     href="https://tgibfnov2022.click/wp-content/plugins/ocean-extra/assets/css/widgets.css?ver=6.2.2" media="all" />
-                <link rel="preload" as="style" id="oe-widgets-style-css"
-                    href="https://tgibfnov2022.click/wp-content/plugins/ocean-extra/assets/css/widgets.css?ver=6.2.2" media="all" />
-                <link rel="preload" as="style" id="sbi_styles-css"
-                    href="https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/css/style.min.css?ver=1.0"
-                    media="all" />
                 <link rel='stylesheet' id='ivory-search-styles-css' href='https://tgibfnov2022.click/wp-content/plugins/add-search-to-menu/public/css/ivory-search.min.css?ver=5.5.1' media='all' />
-                <link rel='stylesheet' id='euis-css' href='https://tgibfnov2022.click/wp-content/plugins/unlimited-elementor-inner-sections-by-boomdevs/public/css/euis-public.css?ver=1.0.0' media='all' />
                 <link rel='stylesheet' id='oceanwp-style-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/css/style.min.css?ver=1.0' media='all' />
-                <link rel='stylesheet' id='child-style-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp-child-theme-master/style.css?ver=6.2.2' media='all' />
                 <link rel='stylesheet' id='content-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp-child-theme-master/css/content.css?ver=6.2.2' media='all' />
                 <link rel='stylesheet' id='header-style-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp-child-theme-master/css/header-style.css?ver=6.2.2' media='all' />
                 <link rel='stylesheet' id='elementor-frontend-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/custom-frontend-lite.min.css?ver=1688107746' media='all' />
                 <link rel='stylesheet' id='elementor-post-38303-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38303.css?ver=1688107746' media='all' />
                 <link rel='stylesheet' id='font-awesome-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/fonts/fontawesome/css/all.min.css?ver=5.15.1' media='all' />
-                <link rel='stylesheet' id='simple-line-icons-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/css/third/simple-line-icons.min.css?ver=2.4.0' media='all' />
                 <link rel='stylesheet' id='oceanwp-google-font-poppins-css' href='https://tgibfnov2022.click/wp-content/uploads/oceanwp-webfonts-css/c60a07be40ef8f258a248a492dd87245.css?ver=6.2.2' media='all' />
                 <link rel='stylesheet' id='elementor-icons-css' href='https://tgibfnov2022.click/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0' media='all' />
-                <link rel='stylesheet' id='swiper-css' href='https://tgibfnov2022.click/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min.css?ver=8.4.5' media='all' />
                 <link rel='stylesheet' id='elementor-post-38164-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38164.css?ver=1688107746' media='all' />
                 <link rel='stylesheet' id='elementor-pro-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/custom-pro-frontend-lite.min.css?ver=1688107746' media='all' />
                 <link rel='stylesheet' id='elementor-global-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/global.css?ver=1688107746' media='all' />
