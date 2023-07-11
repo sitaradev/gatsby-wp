76854c1a5797713f6406f84988ea86306fc2ecdf
gurpreet991 <gurpreet.singh@ellocentlabs.com>
2023-07-11 16:07:57 +0530

    change links location

--- src/templates/page.js
+++ src/templates/page.js
@@ -13,8 +13,46 @@ export default function Page({ pageContext }) {
       <link rel="stylesheet" id="sbi_styles-css"
           href={`https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-${pageId}.css?ver=1688445856`}
           media="all" />
-     
-      </Helmet>
+      <link rel='stylesheet' id="sbi_styles-css"
+          href="https://tgibfnov2022.click/wp-content/plugins/instagram-feed/css/sbi-styles.min.css?ver=6.1.5"
+          media="all" />
+      <link rel='stylesheet' id="wp-block-library-css"
+          href="https://tgibfnov2022.click/wp-includes/css/dist/block-library/style.min.css?ver=6.2.2" media="all" />
+      <link rel='stylesheet' id="classic-theme-styles-css"
+          href="https://tgibfnov2022.click/wp-includes/css/classic-themes.min.css?ver=6.2.2" media="all" />
+      <link rel='stylesheet' id="euis-css"
+          href="https://tgibfnov2022.click/wp-content/plugins/unlimited-elementor-inner-sections-by-boomdevs/public/css/euis-public.css?ver=1.0.0"
+          media="all" />
+      <link rel='stylesheet' id="child-style-css"
+          href="https://tgibfnov2022.click/wp-content/themes/oceanwp-child-theme-master/style.css?ver=6.2.2" media="all" />
+      <link rel='stylesheet' id="simple-line-icons-css"
+          href="https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/css/third/simple-line-icons.min.css?ver=2.4.0"
+          media="all" />
+      <link rel='stylesheet' id="swiper-css"
+          href="https://tgibfnov2022.click/wp-content/plugins/elementor/assets/lib/swiper/v8/css/swiper.min.css?ver=8.4.5"
+          media="all" />
+      <link rel='stylesheet' id="oe-widgets-style-css"
+          href="https://tgibfnov2022.click/wp-content/plugins/ocean-extra/assets/css/widgets.css?ver=6.2.2" media="all" />
+      <link rel='stylesheet' id='ivory-search-styles-css' href='https://tgibfnov2022.click/wp-content/plugins/add-search-to-menu/public/css/ivory-search.min.css?ver=5.5.1' media='all' />
+      <link rel='stylesheet' id='oceanwp-style-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/css/style.min.css?ver=1.0' media='all' />
+      <link rel='stylesheet' id='content-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp-child-theme-master/css/content.css?ver=6.2.2' media='all' />
+      <link rel='stylesheet' id='header-style-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp-child-theme-master/css/header-style.css?ver=6.2.2' media='all' />
+      <link rel='stylesheet' id='elementor-frontend-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/custom-frontend-lite.min.css?ver=1688107746' media='all' />
+      <link rel='stylesheet' id='elementor-post-38303-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38303.css?ver=1688107746' media='all' />
+      {/* <link rel='stylesheet' id='font-awesome-css' href='https://tgibfnov2022.click/wp-content/themes/oceanwp/assets/fonts/fontawesome/css/all.min.css?ver=5.15.1' media='all' /> */}
+      <link rel='stylesheet' id='oceanwp-google-font-poppins-css' href='https://tgibfnov2022.click/wp-content/uploads/oceanwp-webfonts-css/c60a07be40ef8f258a248a492dd87245.css?ver=6.2.2' media='all' />
+      <link rel='stylesheet' id='elementor-icons-css' href='https://tgibfnov2022.click/wp-content/plugins/elementor/assets/lib/eicons/css/elementor-icons.min.css?ver=5.20.0' media='all' />
+      <link rel='stylesheet' id='elementor-post-38164-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38164.css?ver=1688107746' media='all' />
+      <link rel='stylesheet' id='elementor-pro-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/custom-pro-frontend-lite.min.css?ver=1688107746' media='all' />
+      <link rel='stylesheet' id='elementor-global-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/global.css?ver=1688107746' media='all' />
+      <link rel='stylesheet' id='elementor-post-38836-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38836.css?ver=1688109514' media='all' />
+      <link rel='stylesheet' id='oe-widgets-style-css' href='https://tgibfnov2022.click/wp-content/plugins/ocean-extra/assets/css/widgets.css?ver=6.2.2' media='all' />
+      <link rel='stylesheet' id='google-fonts-1-css' href='https://fonts.googleapis.com/css?family=Poppins%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic%7CRoboto+Slab%3A100%2C100italic%2C200%2C200italic%2C300%2C300italic%2C400%2C400italic%2C500%2C500italic%2C600%2C600italic%2C700%2C700italic%2C800%2C800italic%2C900%2C900italic&#038;display=swap&#038;ver=6.2.2' media='all' />
+      <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin />
+      <link rel='stylesheet' id='google-fonts-1-css' href='https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38.css?ver=1688470537' media='all' />
+
+           
+    </Helmet>
       <div id="outer-wrap" className="site clr">
         <div id="wrap">
           <main 
