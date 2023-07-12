a81d4b58641c7c5034aeb33514629e0e53d8b72b
gurpreet991 <gurpreet.singh@ellocentlabs.com>
2023-07-12 18:28:32 +0530

    fixed missing css

--- setup-post-css.js
+++ setup-post-css.js
@@ -1,13 +1,21 @@
 const extractUrls = require("extract-urls");
 const fs = require("fs")
 
-var moreCss = []
+const staticPages = ["https://tgibfnov2022.click/collaborate"]
+const staticLinks = [
+    "https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-38.css",
+    "https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-17000.css?ver=1688729405",
+    "https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-17263.css?ver=1688736520",
+    "https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-20706.css?ver=1688736516",
+    "https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-17321.css?ver=1688736518"
+]
+
 console.log("loading links urls....")
 fetch("https://tgibfnov2022.click/wp-json/wp/v2/pages?orderby=date&_embed")
 .then(res => res.json())
 .then(res => {
-    const promises = []
-    let links = []
+    const promises = [...staticPages]
+    let links = [...staticLinks]
     res.forEach((page) => {
         promises.push(fetch(page.link).then(res => res.text()))
         Promise.all(promises).then(resp => {
@@ -15,7 +23,10 @@ fetch("https://tgibfnov2022.click/wp-json/wp/v2/pages?orderby=date&_embed")
                 links = links.concat(extractUrls(r)).filter(url => url.split("?")[0]?.endsWith(".css"))
 
             })
-            const data = [...new Set(links)].map(url => `@import url("${url}");`).join("\n")
+            const data = [...new Set(links)].map(url => {
+                let u = url.endsWith("'") ? url.substring(0, url.length - 1): url
+                return `@import url("${u}");`
+            }).join("\n")
             fs.writeFile("./src/styles/global.css", data, (err) => {
                 if(!err){
                     console.log("following dynamic css written successfully...")
--- src/templates/page.js
+++ src/templates/page.js
@@ -3,14 +3,18 @@ import { Helmet } from "react-helmet"
 import { Script, link } from "gatsby";
 
 export default function Page({ pageContext }) {
-  const { title, html } = pageContext
+  const { title, html, pageId, guid, id } = pageContext
 
 
   return (
   <>
   <Helmet>
     <title>{title}</title>
-  </Helmet>
+      <link rel="preload" as="style" onload="this.rel='stylesheet'"  id="sbi_styles-css"
+        href={`https://tgibfnov2022.click/wp-content/uploads/elementor/css/post-${pageId}.css?ver=1688736518`}
+        media="all"
+      />
+    </Helmet>
       
   <div id="outer-wrap" className="site clr">
         <div id="wrap">
