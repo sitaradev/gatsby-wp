b6df9b0b75359e5485c8f29e89423f42ca52bb97
gurpreet991 <=>
2023-07-14 12:35:49 +0530

    opacity logic

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,46 +2,6 @@ import "./src/styles/global.css"
 
 import Layout from "./src/components/Layout"
 
-const isAlredyLoaded = (url) => {
-    if (!url) url = "http://xxx.co.uk/xxx/script.js";
-    var scripts = document.getElementsByTagName('script');
-    for (var i = scripts.length; i--;) {
-        if (scripts[i].src == url) return true;
-    }
-    return false;
-}
 
-const addExternalScript = (urls, idx = 0, cb) => {
-    if(idx < urls.length){
-      if(!isAlredyLoaded(urls[idx])){
-        console.log("loading : ", urls[idx])
-        let i = idx
-        const script = document.createElement('script');
-        script.src = urls[idx];
-        script.async=true;
-        script.onload = ()=> addExternalScript(urls, i+1);
-        script.onerror = () => console.log("failed to load : ", urls[idx])
-        document.body.appendChild(script);
-      }
-      else{
-        addExternalScript(urls, idx+1)
-      }
-    }
-    else if(cb) {
-      cb()
-    }
-}
-  
-const scrtpts = [
-  "./inline-scripts/jquery.min.js",
-  "./inline-scripts/webpack.runtime.min.js",
-  "./inline-scripts/frontend-modules.min.js",
-  "./inline-scripts/elementor-frontend-js-before.js",
-  "./inline-scripts/frontend.min.js",
-  "./inline-scripts/owl.min.js",
-  "./inline-scripts/owl.js",
-]
-  
-addExternalScript(scrtpts)
 
 export const wrapPageElement =  Layout;
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,12 +1,56 @@
 import * as React from "react";
 import { Helmet } from "react-helmet"
-// import { script, link } from "gatsby";
 
+const isAlredyLoaded = (url) => {
+  if (!url) return true
+  var scripts = document.getElementsByTagName('script');
+  for (var i = scripts.length; i--;) {
+      if (scripts[i].src == url) return true;
+  }
+  return false;
+}
+
+const addExternalScript = (urls, idx = 0, cb) => {
+  if(idx < urls.length){
+    if(!isAlredyLoaded(urls[idx])){
+      console.log("loading : ", urls[idx])
+      let i = idx
+      const script = document.createElement('script');
+      script.src = urls[idx];
+      script.async=true;
+      script.onload = ()=> addExternalScript(urls, i+1, cb);
+      script.onerror = () => console.log("failed to load : ", urls[idx])
+      document.body.appendChild(script);
+    }
+    else{
+      addExternalScript(urls, idx+1, cb)
+    }
+  }
+  else if(cb) {
+    cb()
+  }
+}
+
+const scrtpts = [
+"./inline-scripts/jquery.min.js",
+"./inline-scripts/webpack.runtime.min.js",
+"./inline-scripts/frontend-modules.min.js",
+"./inline-scripts/elementor-frontend-js-before.js",
+"./inline-scripts/frontend.min.js",
+"./inline-scripts/owl.min.js",
+"./inline-scripts/owl.js",
+]
 
 export default function Page({ pageContext }) {
   const { title, html, pageId, guid, id } = pageContext
+  const [load, setLoad] = React.useState(false)
 
-  
+  addExternalScript(scrtpts, 0, () => {
+    setTimeout(() => {
+      setLoad(true)
+    },10)
+  })
+ 
   return (
   <>
     <Helmet>
@@ -19,7 +63,7 @@ export default function Page({ pageContext }) {
 
     </Helmet>
       
-    <div id="outer-wrap" className="site clr">
+    <div id="outer-wrap" className="site clr" style={{ opacity: load ? 1 : 0 }}>
       <div id="wrap">
         <main 
           id="main" 
